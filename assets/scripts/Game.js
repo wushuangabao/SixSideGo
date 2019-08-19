7d4515c6438ef69a53ae1acb8cf20d98f3166551
无双BaOY_WHA <445395697@qq.com>
2019-08-19 21:59:08 +0800

    完成计时、认输、停一手

--- assets/scripts/Game.js
+++ assets/scripts/Game.js
@@ -4,7 +4,7 @@ cc.Class({
     properties: {
         overSprite: {
             default: null,
-            type: cc.Sprite,
+            type: cc.Node,
         },
         overLabel: {
             default: null,
@@ -42,20 +42,52 @@ cc.Class({
             default: null,
             type: cc.SpriteFrame
         },
+        UI1: {
+            default: null,
+            type: cc.Node
+        },
+        UI2: {
+            default: null,
+            type: cc.Node
+        },
+        daojishi: {
+            default: 30,
+            displayName: "倒计时（秒）"
+        },
+        danji: {
+            default: true,
+            displayName: "是否单机"
+        },
         touchChess: {  // 每一回合落下的棋子
             default: null,
             type: cc.Node,
             visible: false  // 属性窗口不显示
         },
-        gameState: 'blue',
     },
+
     // 重新开始
     startGame() {
-        cc.director.loadScene("Game");
+        // cc.director.loadScene("game");
     },
-    // 返回菜单
-    toMenu() {
-        cc.director.loadScene("Menu");
+
+
+    // 初始化游戏参数
+    initializeParams() {
+        this.strOver = '';
+        this.seconds = 0;
+        this.size = 13; // 棋盘边长与小六边形边长的比值
+        this.maxY = 18; // 行数
+        this.maxX = 27; // 列数
+        this.overSprite.x = 10000; // 让结束画面位于屏幕外
+        this.overLabel.string = '';
+        this.labelDaoJiShi.string = '' + this.daojishi;
+        this.UiDaoJiShi.color = new cc.Color(0, 125, 255);
+        this.jie = {  // 记录劫点
+            red: null,
+            blue: null,
+            tag: 0
+        };
+        this.gameState = 'blue';
     },
 
     // 调整画面宽高
@@ -64,7 +96,7 @@ cc.Class({
         this.canvasNode = cvs.node;
         this.curDR1 = cvs.designResolution;  // Canvas的设计分辨率
         var dr = this.curDR1;
-        var s = cc.view.getFrameSize();  //在 native 平台下，它返回全屏视图下屏幕的尺寸
+        var s = cc.view.getFrameSize();  // 在 native 平台下，它返回全屏视图下屏幕的尺寸
         var rw = s.width;
         var rh = s.height;
         var finalW = rw;
@@ -281,7 +313,7 @@ cc.Class({
         if (chess.tagColor != color) {
             if (chess.tagColor == 'null')
                 return true;
-            else{
+            else {
                 this.jie.tag -= 2;  // 劫的标记-2
                 return false;
             }
@@ -295,13 +327,7 @@ cc.Class({
             if (len > 0) {
                 for (var i = 0; i < len; i++) {
                     let chess = this.chessList[this.listTagNum[i]];
-                    if (chess.tagType == 'A') {
-                        chess.getComponent(cc.Sprite).spriteFrame = this.nullSpriteFrameA;
-                    }
-                    else {
-                        chess.getComponent(cc.Sprite).spriteFrame = this.nullSpriteFrameB;
-                    }
-                    chess.tagColor = 'null';
+                    this.setChess(chess, 'null');
                 }
                 if (len == 1) {
                     this.jie.tag += 1;  // 劫的标记+1
@@ -312,13 +338,27 @@ cc.Class({
         return !isAlive;
     },
 
+    setChess(chess, str) {
+        if(!chess)
+            return;
+        if (str === 'null') {
+            if (chess.tagType == 'A') {
+                chess.getComponent(cc.Sprite).spriteFrame = this.nullSpriteFrameA;
+            }
+            else {
+                chess.getComponent(cc.Sprite).spriteFrame = this.nullSpriteFrameB;
+            }
+            chess.tagColor = str;
+        }
+    },
+
     // 点击某落子点 的回调函数
     onTouchChess(event) {
         var self = event.currentTarget;
         if (self.tagColor != 'null')
             return;
         // 考察是否为劫点
-        if(self.tagNum == this.jie[this.gameState])
+        if (self.tagNum == this.jie[this.gameState])
             return;
         // 考察周围是否可以提子（直接提走）
         var fanState = this.fanState();
@@ -360,10 +400,22 @@ cc.Class({
                 self.getComponent(cc.Sprite).spriteFrame = this.blueSpriteFrameA;
             }
         }
-        // 劫点清除
-        this.jie[this.gameState] = null;
-        // 切换颜色
-        this.gameState = this.fanState();
+        this.changeGameState();
+    },
+
+    // 切换颜色
+    changeGameState() {
+        this.jie[this.gameState] = null;  // 劫点清除
+        if (this.gameState == 'red') {
+            this.UiDaoJiShi.color = new cc.Color(0, 125, 255);
+            this.gameState = 'blue';
+        }
+        else {
+            this.UiDaoJiShi.color = new cc.Color(255, 25, 0);
+            this.gameState = 'red'
+        }
+        this.labelDaoJiShi.string = "" + this.daojishi;
+        this.seconds = 0;
     },
 
     fanState() {
@@ -374,43 +426,48 @@ cc.Class({
     },
 
     // 根据落子点在chessList中的索引值，计算落子点的物理坐标
-    ccV2ById(x, y) {
+    ccV2ById(x, y, dx, dy) {
         var w = this.w,
-            cx = this.curDR2.width / 2,
-            cy = this.curDR2.height / 2,
-            xx = Math.sqrt(3) / 2 * w * x - cx + this.dBorderX,
-            yy = 1.5 * w * y - cy + this.dBorderY;
+            cx = dx || (this.dBorderX - this.curDR2.width / 2),
+            cy = dy || (this.dBorderY - this.curDR2.height / 2),
+            xx = Math.sqrt(3) / 2 * w * x + cx,
+            yy = 1.5 * w * y + cy;
         return cc.v2(xx, yy);
     },
 
     onLoad() {
-        this.chessList = new Array();
-        this.size = 13; // 棋盘边长与小六边形边长的比值
-        this.maxY = 18; // 行数
-        this.maxX = 27; // 列数
-        this.overSprite.node.x = 10000; // 让结束画面位于屏幕外
-        this.jie = {  // 记录劫点
-            red: null,
-            blue: null,
-            tag: 0
-        };
+        this.UiDaoJiShi = this.UI2.getChildByName("DaoJiShi");
+        this.labelDaoJiShi = this.UiDaoJiShi.getComponent(cc.Label);
+        this.initializeParams();
         this.resize();
-        var self = this;
-        var boardZhanBi = 10 / 11; // 棋盘宽度占画面宽度的比例
-        var w1 = this.curDR2.width * boardZhanBi / Math.sqrt(3),
-            w2 = this.curDR2.height * boardZhanBi * 0.5;
-        if (w1 > w2) {
-            this.w = w2 / this.size;
-            this.dBorderX = this.curDR1.width * 0.5 - 0.5 * Math.sqrt(3) * w2;
-            this.dBorderY = this.curDR1.height * 0.5 - 0.75 * w2
-        } else {
-            this.dBorderX = this.curDR1.width * 0.5 - 0.5 * Math.sqrt(3) * w1;
-            this.dBorderY = this.curDR1.height * 0.5 - 0.75 * w1
-            this.w = w1 / this.size;
-        }
-        var scaleNode = this.w / 200;
+        var self = this,
+            sqrt3 = Math.sqrt(3),
+            boardZhanBi = 10 / 11, // 棋盘宽度占画面宽度的比例
+            w0 = this.curDR2.width * boardZhanBi;
+        this.dBorderX = 0.5 * (this.curDR2.width - w0);
+        w0 /= sqrt3;
+        this.dBorderY = 0.5 * this.curDR2.height - w0;
+        this.w = w0 / this.size;
+        var scaleNode = this.w / 200,
+            dx = this.dBorderX - this.curDR2.width * 0.5,
+            dy = this.dBorderY - this.curDR2.height * 0.5;
+
+        // 调整棋盘边界的位置
+        var borderW = this.size * this.w,
+            nodeBorder = this.node.getChildByName("Border");
+        nodeBorder.setPosition(dx + 0.5 * sqrt3 * borderW, dy + borderW);
+        // 落子点格子与边界的错位修正值
+        dy += 7;
+
+        // 设置 UI 的位置等
+        // todo: 考虑屏幕宽度大于高度的情形
+        var yUi = (this.curDR2.height + borderW) * 0.33333;
+        this.UI1.setPosition(0, -yUi);
+        this.UI2.setPosition(0, yUi);
+        this.labelDaoJiShi.string = "" + this.daojishi;
 
         // 初始化棋盘上的点，并为每个节点添加事件
+        this.chessList = new Array();
         for (var y = 0; y < this.maxY; y++) {
             for (var x = 0; x < this.maxX; x++) {
                 if (this.isInBoard(x, y, true)) {
@@ -426,7 +483,7 @@ cc.Class({
                     newNode.setScale(scaleNode);  // 缩放到合适大小
                     this.node.addChild(newNode);
                     this.chessList.push(newNode);
-                    newNode.setPosition(this.ccV2ById(x, y));  // 使每个节点位于指定位置
+                    newNode.setPosition(this.ccV2ById(x, y, dx, dy));  // 使每个节点位于指定位置
                     newNode.tagNum = this.maxX * y + x;
                     newNode.tagColor = 'null';
                     // 添加边界标签。  1-6：从左上的边开始，逆时针排列。 其他暂时设置为0
@@ -440,106 +497,82 @@ cc.Class({
                 }
             }
         }
-        this.gameState = 'blue';
     },
 
-    judgeOver() {
-        var x0 = this.touchChess.tagNum % this.maxX;
-        var y0 = parseInt(this.touchChess.tagNum / this.maxX);
-        //判断横向
-        var fiveCount = 0;
-        for (var x = 0; x < 15; x++) {
-            if ((this.chessList[y0 * 15 + x].getComponent(cc.Sprite)).spriteFrame === this.touchChess.getComponent(cc.Sprite).spriteFrame) {
-                fiveCount++;
-                if (fiveCount == 5) {
-                    if (this.gameState === 'black') {
-                        this.overLabel.string = "你赢了";
-                        this.overSprite.node.x = 0;
-                    } else {
-                        this.overLabel.string = "你输了";
-                        this.overSprite.node.x = 0;
-                    }
-                    this.gameState = 'over';
-                    return;
-                }
-            } else {
-                fiveCount = 0;
+    // 悔棋 - 按钮点击回调函数
+    huiqi() {
+        // todo: 发送悔棋的请求给对手
+        if (this.danji) {
+            // 返回上一步的状态
+            this.overLabel.string = this.getColorHanZi(this.gameState) + '请求悔棋';
+            this.tagOverLabel = 2; // 显示 2 秒
+        }
+    },
+
+    // 认输 - 按钮点击回调函数
+    renshu() {
+        this.strOver = this.getColorHanZi(this.gameState) + '认输，';
+        this.gameOver(this.fanState());
+    },
+
+    // 停一手 - 按钮点击回调函数
+    tingyishou() {
+        this.overLabel.string = this.getColorHanZi(this.gameState) + '停一手';
+        this.tagOverLabel = 2; // 显示 2 秒
+        this.changeGameState();
+    },
+
+    getColorHanZi(color) {
+        if (color == 'red')
+            return '红方';
+        else if (color == 'blue')
+            return '蓝方';
+        else return 'null';
+    },
+
+    // color 为胜利方
+    gameOver(color) {
+        if (!color)
+            var color = this.judgeWinner();
+        this.overLabel.string = this.strOver + this.getColorHanZi(color) + '获胜';
+        this.tagOverLabel = 30; // 显示 30 秒
+        this.overSprite.x = 0;
+        this.labelDaoJiShi.string = '';
+    },
+
+    // 重新开始 - 按钮点击回调函数
+    restart() {
+        this.tagOverLabel = 0;
+        this.initializeParams();
+        for (var i = 0; i < this.chessList.length; ++i) {
+            let chess = this.chessList[i];
+            if (chess && chess.tagColor) {
+                this.setChess(chess, 'null');
             }
         }
-        //判断纵向
-        fiveCount = 0;
-        for (var y = 0; y < 15; y++) {
-            if ((this.chessList[y * 15 + x0].getComponent(cc.Sprite)).spriteFrame === this.touchChess.getComponent(cc.Sprite).spriteFrame) {
-                fiveCount++;
-                if (fiveCount == 5) {
-                    if (this.gameState === 'black') {
-                        this.overLabel.string = "你赢了";
-                        this.overSprite.node.x = 0;
-                    } else {
-                        this.overLabel.string = "你输了";
-                        this.overSprite.node.x = 0;
-                    }
-                    this.gameState = 'over';
-                    return;
+    },
+
+    update(dt) {
+        if(this.labelDaoJiShi.string != '')
+            this.seconds += dt;
+        // 每隔 1 秒
+        if (this.seconds >= 1) {
+            this.seconds = 0;
+            let daojishi = Number(this.labelDaoJiShi.string) - 1;
+            if (daojishi < 0) {
+                this.labelDaoJiShi.string = '' + this.daojishi;
+                this.strOver = this.getColorHanZi(this.gameState) + '超时，';
+                this.gameOver(this.fanState());
+            }
+            else this.labelDaoJiShi.string = '' + daojishi;
+            // 如果 overLabel 存在显示文字
+            if (this.tagOverLabel) {
+                this.tagOverLabel -= 1;
+                if (this.tagOverLabel <= 0) {
+                    this.overLabel.string = '';
+                    this.strOver = '';
                 }
-            } else {
-                fiveCount = 0;
             }
-        }
-        //判断右上斜向
-        var f = y0 - x0;
-        fiveCount = 0;
-        for (var x = 0; x < 15; x++) {
-            if (f + x < 0 || f + x > 14) {
-                continue;
-            }
-            if ((this.chessList[(f + x) * 15 + x].getComponent(cc.Sprite)).spriteFrame === this.touchChess.getComponent(cc.Sprite).spriteFrame) {
-                fiveCount++;
-                if (fiveCount == 5) {
-                    if (this.gameState === 'black') {
-                        this.overLabel.string = "你赢了";
-                        this.overSprite.node.x = 0;
-                    } else {
-                        this.overLabel.string = "你输了";
-                        this.overSprite.node.x = 0;
-                    }
-                    this.gameState = 'over';
-                    return;
-                }
-            } else {
-                fiveCount = 0;
-            }
-        }
-        //判断右下斜向
-        f = y0 + x0;
-        fiveCount = 0;
-        for (var x = 0; x < 15; x++) {
-            if (f - x < 0 || f - x > 14) {
-                continue;
-            }
-            if ((this.chessList[(f - x) * 15 + x].getComponent(cc.Sprite)).spriteFrame === this.touchChess.getComponent(cc.Sprite).spriteFrame) {
-                fiveCount++;
-                if (fiveCount == 5) {
-                    if (this.gameState === 'black') {
-                        this.overLabel.string = "你赢了";
-                        this.overSprite.node.x = 0;
-                    } else {
-                        this.overLabel.string = "你输了";
-                        this.overSprite.node.x = 0;
-                    }
-                    this.gameState = 'over';
-                    return;
-                }
-            } else {
-                fiveCount = 0;
-            }
-        }
-        //没有输赢交换下子顺序
-        if (this.gameState === 'black') {
-            this.gameState = 'white';
-        } else {
-            this.gameState = 'black';
         }
     }
-
 });
